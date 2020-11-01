3fed238290a91a89fb09d5d32178d54088336b39
karkoon <kacperak23@gmail.com>
2020-11-01 21:16:12 +0100

    Poprawa testu integracyjnego. Dodanie komentarzy lepiej opisujących fragmenty zadania do odpowiedniego kodu testów.

--- src/test/agh/cs/oop/WorldIT.java
+++ src/test/agh/cs/oop/WorldIT.java
@@ -10,17 +10,20 @@ public class WorldIT {
 
   @Test
   public void normalArguments() {
-    String[] args = "f f r l zaba l l l r test f b".split(" ");
+    String[] args = "f f f f f r l zaba l l l r test f b".split(" ");
     Animal animal = new Animal();
     assertEquals("Orientacja: Północ, Pozycja: (2,2)", animal.toString());
     OptionsParser parser = new OptionsParser();
     MoveDirection[] directions = parser.parse(args);
     assertArrayEquals(
-      new MoveDirection[]{FORWARD, FORWARD, RIGHT, LEFT, LEFT, LEFT, LEFT, RIGHT, FORWARD, BACKWARD},
-      directions);
-    String[] positions = {
+      new MoveDirection[]{FORWARD, FORWARD, FORWARD, FORWARD, FORWARD, RIGHT, LEFT, LEFT, LEFT, LEFT, RIGHT, FORWARD, BACKWARD},
+      directions); // "czy dane wejściowe podane jako tablica łańcuchów znaków są poprawnie interpretowane."
+    String[] positions = { // "czy zwierzę ma właściwą orientację", "czy zwierzę przemieszcza się na właściwe pozycje"
       "Orientacja: Północ, Pozycja: (2,3)",
       "Orientacja: Północ, Pozycja: (2,4)",
+      "Orientacja: Północ, Pozycja: (2,4)",//
+      "Orientacja: Północ, Pozycja: (2,4)",// "czy zwierzę nie wychodzi poza mapę"
+      "Orientacja: Północ, Pozycja: (2,4)",//
       "Orientacja: Wschód, Pozycja: (2,4)",
       "Orientacja: Północ, Pozycja: (2,4)",
       "Orientacja: Zachód, Pozycja: (2,4)",
